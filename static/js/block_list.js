6e4c2e40e4162d60de8f99ce8d58c1c87962b18f
yuta308 <63042906+yuta308@users.noreply.github.com>
2023-11-22 13:50:10 +0900

    テスト

---
+++ static/js/block_list.js
@@ -0,0 +1,248 @@
+Blockly.defineBlocksWithJsonArray(
+  /* ここにJSONファイルの内容をコピー */
+  [{
+    "type": "loop_counts",
+    "message0": "%1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "loop_counts",
+        "text": "1"
+      }
+    ],
+    "output": "Number",
+    "colour": 120,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    'type': 'controls_repeat_ext',
+    'message0': '%{BKY_CONTROLS_REPEAT_TITLE}',
+    'args0': [{
+      'type': 'input_value',
+      'name': 'TIMES',
+      'check': 'Number',
+    }],
+    'message1': '%{BKY_CONTROLS_REPEAT_INPUT_DO} %1',
+    'args1': [{
+      'type': 'input_statement',
+      'name': 'DO',
+    }],
+    'previousStatement': null,
+    'nextStatement': null,
+    'style': 'loop_blocks',
+    'tooltip': '%{BKY_CONTROLS_REPEAT_TOOLTIP}',
+    'helpUrl': '%{BKY_CONTROLS_REPEAT_HELPURL}',
+  },
+  {
+    "type": "logic_sample2",
+    "message0": "文字列 %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "logic_sample_Japanese",
+        "text": "こんにちは"
+      }
+    ],
+    "output": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "logic_sample",
+    "message0": "値を確認 %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "logic_sample",
+        "text": "A1"
+      }
+    ],
+    "output": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    'type': 'logic_compare',
+    'message0': '%1 %2 %3',
+    'args0': [
+      {
+        'type': 'input_value',
+        'name': 'A',
+      },
+      {
+        'type': 'field_dropdown',
+        'name': 'OP',
+        'options': [
+          ['=', '=='],
+          ['\u2260', '=!'],
+          ['\u200F<', 'LT'],
+          ['\u200F\u2264', 'LTE'],
+          ['\u200F>', 'GT'],
+          ['\u200F\u2265', 'GTE'],
+        ],
+      },
+      {
+        'type': 'input_value',
+        'name': 'B',
+      },
+    ],
+    'inputsInline': true,
+    'output': 'Boolean',
+    'style': 'logic_blocks',
+    'helpUrl': '%{BKY_LOGIC_COMPARE_HELPURL}',
+    'extensions': ['logic_compare', 'logic_op_tooltip'],
+  },
+
+  {
+    "type": "function_name",
+    "message0": "関数名 %1 %2",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "func_name",
+        "text": "default"
+      },
+      {
+        "type": "input_statement",
+        "name": "func",
+        "check": "String"
+      }
+    ],
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "get_info",
+    "message0": "スプレッドシートの情報を取得",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "getrange",
+    "message0": "セルの範囲 %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "getRange",
+        "text": "A1"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "setValue",
+    "message0": "入力する値 %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "setValue",
+        "text": "hello_world"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "program_run",
+    "message0": "実行する",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    'type': 'controls_if',
+    'message0': '%{BKY_CONTROLS_IF_MSG_IF} %1',
+    'args0': [
+      {
+        'type': 'input_value',
+        'name': 'IF0',
+        'check': '',
+      },
+    ],
+    'message1': '%{BKY_CONTROLS_IF_MSG_THEN} %1',
+    'args1': [
+      {
+        'type': 'input_statement',
+        'name': 'DO0',
+      },
+    ],
+    'previousStatement': null,
+    'nextStatement': null,
+    'style': 'logic_blocks',
+    'helpUrl': '%{BKY_CONTROLS_IF_HELPURL}',
+    'suppressPrefixSuffix': true,
+    'mutator': 'controls_if_mutator',
+    'extensions': ['controls_if_tooltip'],
+  },
+  {
+    "type": "get_date",
+    "message0": "時間データを取得",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "hour",
+    "message0": "時データを取得",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "min",
+    "message0": "分情報を取得",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "day",
+    "message0": "日付データを取得",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "month",
+    "message0": "月のデータを取得",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "week",
+    "message0": "週の曜日データを取得",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  }
+]
+);
--- static/js/myblock.js
+++ static/js/myblock.js
@@ -1,254 +1,3 @@
-Blockly.defineBlocksWithJsonArray(
-  /* ここにJSONファイルの内容をコピー */
-  [{
-    "type": "loop_counts",
-    "message0": "%1",
-    "args0": [
-      {
-        "type": "field_input",
-        "name": "loop_counts",
-        "text": "1"
-      }
-    ],
-    "output": "Number",
-    "colour": 120,
-    "tooltip": "",
-    "helpUrl": ""
-  }
-    , {
-    'type': 'controls_repeat_ext',
-    'message0': '%{BKY_CONTROLS_REPEAT_TITLE}',
-    'args0': [{
-      'type': 'input_value',
-      'name': 'TIMES',
-      'check': 'Number',
-    }],
-    'message1': '%{BKY_CONTROLS_REPEAT_INPUT_DO} %1',
-    'args1': [{
-      'type': 'input_statement',
-      'name': 'DO',
-    }],
-    'previousStatement': null,
-    'nextStatement': null,
-    'style': 'loop_blocks',
-    'tooltip': '%{BKY_CONTROLS_REPEAT_TOOLTIP}',
-    'helpUrl': '%{BKY_CONTROLS_REPEAT_HELPURL}',
-  },
-    , {
-    "type": "logic_sample2",
-    "message0": "文字列 %1",
-    "args0": [
-      {
-        "type": "field_input",
-        "name": "logic_sample_Japanese",
-        "text": "こんにちは"
-      }
-    ],
-    "output": null,
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  }
-    , {
-    "type": "logic_sample",
-    "message0": "値を確認 %1",
-    "args0": [
-      {
-        "type": "field_input",
-        "name": "logic_sample",
-        "text": "A1"
-      }
-    ],
-    "output": null,
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  }
-    , {
-    'type': 'logic_compare',
-    'message0': '%1 %2 %3',
-    'args0': [
-      {
-        'type': 'input_value',
-        'name': 'A',
-      },
-      {
-        'type': 'field_dropdown',
-        'name': 'OP',
-        'options': [
-          ['=', '=='],
-          ['\u2260', '=!'],
-          ['\u200F<', 'LT'],
-          ['\u200F\u2264', 'LTE'],
-          ['\u200F>', 'GT'],
-          ['\u200F\u2265', 'GTE'],
-        ],
-      },
-      {
-        'type': 'input_value',
-        'name': 'B',
-      },
-    ],
-    'inputsInline': true,
-    'output': 'Boolean',
-    'style': 'logic_blocks',
-    'helpUrl': '%{BKY_LOGIC_COMPARE_HELPURL}',
-    'extensions': ['logic_compare', 'logic_op_tooltip'],
-  },
-
-  {
-    "type": "function_name",
-    "message0": "関数名 %1 %2",
-    "args0": [
-      {
-        "type": "field_input",
-        "name": "func_name",
-        "text": "default"
-      },
-      {
-        "type": "input_statement",
-        "name": "func",
-        "check": "String"
-      }
-    ],
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  },
-  {
-    "type": "get_info",
-    "message0": "スプレッドシートの情報を取得",
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  },
-  {
-    "type": "getrange",
-    "message0": "セルの範囲 %1",
-    "args0": [
-      {
-        "type": "field_input",
-        "name": "getRange",
-        "text": "A1"
-      }
-    ],
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  },
-  {
-    "type": "setValue",
-    "message0": "入力する値 %1",
-    "args0": [
-      {
-        "type": "field_input",
-        "name": "setValue",
-        "text": "hello_world"
-      }
-    ],
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  },
-  {
-    "type": "program_run",
-    "message0": "実行する",
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  },
-  {
-    'type': 'controls_if',
-    'message0': '%{BKY_CONTROLS_IF_MSG_IF} %1',
-    'args0': [
-      {
-        'type': 'input_value',
-        'name': 'IF0',
-        'check': '',
-      },
-    ],
-    'message1': '%{BKY_CONTROLS_IF_MSG_THEN} %1',
-    'args1': [
-      {
-        'type': 'input_statement',
-        'name': 'DO0',
-      },
-    ],
-    'previousStatement': null,
-    'nextStatement': null,
-    'style': 'logic_blocks',
-    'helpUrl': '%{BKY_CONTROLS_IF_HELPURL}',
-    'suppressPrefixSuffix': true,
-    'mutator': 'controls_if_mutator',
-    'extensions': ['controls_if_tooltip'],
-  },
-  {
-    "type": "get_date",
-    "message0": "時間データを取得",
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  },
-  {
-    "type": "hours",
-    "message0": "時データを取得",
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  },
-  {
-    "type": "min",
-    "message0": "分を取得",
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  },
-  {
-    "type": "day",
-    "message0": "日付データを取得",
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  },
-  {
-    "type": "month",
-    "message0": "月のデータを取得",
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  },
-  {
-    "type": "week",
-    "message0": "週の曜日データを取得",
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  },
-  ]
-);
-
-
-
 javascript.javascriptGenerator.forBlock['function_name'] = function (block, generator) {
   var text_func_name = block.getFieldValue('func_name');
   var statements_func = generator.statementToCode(block, 'func');
@@ -372,7 +121,7 @@ javascript.javascriptGenerator.forBlock['get_date'] = function (block, generator
   this.setNextStatement(true, null);
   // TODO: Assemble javascript into code variable.
   var code = '    var date  =  new Date();<br>';
-  const outputElement = document.getElementById("get_date");
+  const outputElement = document.getElementById("output");
   outputElement.innerHTML = code;
   return code;
 };
@@ -381,15 +130,16 @@ javascript.javascriptGenerator.forBlock['hours'] = function (block, generator) {
   this.setNextStatement(true, null);
   // TODO: Assemble javascript into code variable.
   var code = '    var hours = date.getHours();<br>';
-  const outputElement = document.getElementById("hours");
+  const outputElement = document.getElementById("output");
   outputElement.innerHTML = code;
   return code;
 };
+
 javascript.javascriptGenerator.forBlock['min'] = function (block, generator) {
   this.setNextStatement(true, null);
   // TODO: Assemble javascript into code variable.
   var code = '    var min = date.getMinutes();<br>';
-  const outputElement = document.getElementById("min");
+  const outputElement = document.getElementById("output");
   outputElement.innerHTML = code;
   return code;
 };
@@ -397,7 +147,7 @@ javascript.javascriptGenerator.forBlock['day'] = function (block, generator) {
   this.setNextStatement(true, null);
   // TODO: Assemble javascript into code variable.
   var code = '    var day = date.getDate();<br>';
-  const outputElement = document.getElementById("day");
+  const outputElement = document.getElementById("output");
   outputElement.innerHTML = code;
   return code;
 };
@@ -405,17 +155,17 @@ javascript.javascriptGenerator.forBlock['month'] = function (block, generator) {
   this.setNextStatement(true, null);
   // TODO: Assemble javascript into code variable.
   var code = '    var month = date.getMonth() + 1;<br>';
-  const outputElement = document.getElementById("month");
+  const outputElement = document.getElementById("output");
   outputElement.innerHTML = code;
   return code;
 };
 javascript.javascriptGenerator.forBlock['week'] = function (block, generator) {
   this.setNextStatement(true, null);
   // TODO: Assemble javascript into code variable.
-  var code = "      var daysOfWeek = ['日', '月', '火', '水', '木', '金', '土'];<br>"
-  code += "    var week_num =date.getDay()<br>;"
+  var code = "      var daysOfWeek = ['日', '月', '火', '水', '木', '金', '土'];<br>";
+  code += "    var week_num =date.getDay()<br>;";
   code += "    var week = daysOfWeek[week_num];<br>";
-  const outputElement = document.getElementById("week");
+  const outputElement = document.getElementById("output");
   outputElement.innerHTML = code;
   return code;
 };
